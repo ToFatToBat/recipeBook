d0cf68d345004ef418f86111c4bcc3b19695a60f
ToFatToBat <kpapiernia@wp.pl>
2020-09-29 00:42:36 +0200

    Dodanie funkcjinalnosci wyboru kategorii

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,5 +4,5 @@ INSERT INTO Category (id, name, description) VALUES
     (3, 'Zupy', 'Zupy gotowane na miesie');
 
 INSERT INTO Recipe (id, name, description, likes, url, category_id) VALUES
-    (1, 'Sernik', 'twarog, biszkopty, truskawki, galaretka truskawkowa, zelatyna, mleko, śmietanka, cukier wanilinowy', 2, 'https://wszystkiegoslodkiego.pl/storage/images/201930/sernik-z-polewa-czekoladowa-1.jpg', 1 ),
+    (1, 'Sernik', 'twarog, biszkopty, truskawki, galaretka truskawkowa, zelatyna, mleko, śmietanka, cukier wanilinowy', 2, 'https://wszystkiegoslodkiego.pl/storage/images/201930/sernik-z-polewa-czekoladowa-1.jpg', 2 ),
     (2, 'Pomidorowa', 'Pomidory, woda', 4, 'https://s3.przepisy.pl/przepisy3ii/img/variants/800x0/zupa-pomidorowa.jpg', 3);
